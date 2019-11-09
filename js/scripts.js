e922bef1942f8eb3a52935add6a5bdf73669480b
Scyzoryk <damianszefler96@gmail.com>
2019-11-09 20:21:11 +0100

    finalizacja zegara

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    var time =
+        '<div class="tarcza zaokTOP zaokBOTTOM blush">'+h+'</div>:<div class="tarcza zaokTOP zaokBOTTOM blush">'+m+'</div>:<div class="tarcza zaokTOP zaokBOTTOM blush">'+s+'</div>'
+    document.getElementById('zegar').innerHTML = time;
+    var t = setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i}
+    return i;
+}
