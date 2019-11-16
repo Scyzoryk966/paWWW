5a7a2a819f1a14c2043e1cf8f4b874364f55e5eb
Scyzoryk <damianszefler96@gmail.com>
2019-11-17 00:52:57 +0100

    dodane vid/mp3 sociale, poprawione stopki i menu

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,14 @@ function startTime() {
     m = checkTime(m);
     s = checkTime(s);
     var time =
-        '<div class="tarcza zaokTOP zaokBOTTOM blush">'+h+'</div>:<div class="tarcza zaokTOP zaokBOTTOM blush">'+m+'</div>:<div class="tarcza zaokTOP zaokBOTTOM blush">'+s+'</div>'
+        '<div class="tarcza zaokTOP zaokBOTTOM blush">' + h + '</div>:<div class="tarcza zaokTOP zaokBOTTOM blush">' + m + '</div>:<div class="tarcza zaokTOP zaokBOTTOM blush">' + s + '</div>'
     document.getElementById('zegar').innerHTML = time;
     var t = setTimeout(startTime, 1000);
 }
 
 function checkTime(i) {
-    if (i < 10) {i = "0" + i}
+    if (i < 10) {
+        i = "0" + i
+    }
     return i;
 }
